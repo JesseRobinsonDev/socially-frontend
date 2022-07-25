8d4d5610360c843aa49ea0abeb63dd47e5a32eb2
mendingbook <jesse.robinson004@gmail.com>
2022-07-24 20:56:06 -0500

    Set up Page Layout & Header component

---
+++ components/layout/PageLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-layout">
+    <PageHeader />
+    <div class="page-children"><slot /></div>
+  </div>
+</template>
+
+<script>
+import PageHeader from './PageHeader.vue'
+export default {
+  name: 'PageLayout',
+  components: { PageHeader },
+}
+</script>
+
+<style>
+.page-layout {
+}
+
+.page-children {
+  padding-left: 4rem;
+  padding-right: 4rem;
+}
+</style>
